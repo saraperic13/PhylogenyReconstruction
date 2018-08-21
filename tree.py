eb20862061bdceab2007287e6c62d021db004d13
saraperic13 <sara.peric013@gmail.com>
2018-08-21 18:36:44 -0500

    Retrieve random node and random two of its descendants implemented.

--- tree.py
+++ tree.py
@@ -1,3 +1,7 @@
+import random
+import numpy as np
+
+
 class Tree:
 
     def __init__(self, number_of_named, nodes=[]):
@@ -13,6 +17,25 @@ class Tree:
     def increment_number_of_named(self):
         self.number_of_named += 1
 
-    def __str__(self):
-        for node in self.nodes:
-            print("Children of node ", node.name, [n.name for n in node.descendants])
+    # def __str__(self):
+    #     for node in self.nodes:
+    #         print("Children of node ", node.name, [n.name for n in node.descendants])
+
+    def get_random_node(self):
+        subroot = random.choice(self.nodes)
+        while not subroot.descendants:
+            subroot = random.choice(self.nodes)
+        return subroot
+
+    def get_all_descendants(self, node, descendants):
+        if not node.descendants:
+            return
+        for item in node.descendants:
+            descendants.append(item)
+        for child in node.descendants:
+            self.get_all_descendants(child, descendants)
+
+    def get_random_descendants_from_subroot(self, subroot):
+        descendants = []
+        self.get_all_descendants(subroot, descendants)
+        return descendants if not descendants else random.choice(descendants), random.choice(descendants)
