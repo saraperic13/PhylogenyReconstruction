0d8ea2ca933caa2bf941a9d1c7e9d2bca07d302c
saraperic13 <sara.peric013@gmail.com>
2018-08-27 14:18:48 -0500

    Batch sized model.

--- encoder.py
+++ encoder.py
@@ -11,9 +11,9 @@ dnaNumLetters = 4
 
 learningRate = 0.05
 
-batchSize = 100
+batchSize = 10
 
-numTrainingIters = 1000
+numTrainingIters = 50
 
 
 def sequence_to_one_hot_enc(seq):
@@ -47,29 +47,28 @@ def read_data(file_name):
     return data
 
 
-def get_dna_sequences(data):
-    myInts = np.random.random_integers(0, len(data) - 1, batchSize)
-    return np.stack(data[i] for i in myInts.flat)
-
-
 def get_subroot_and_nodes(tree, data):
-    # for i in range(batchSize):
-    subroot = tree.get_random_node()
-    descendants = []
-    tree_utils.get_all_node_descendant_leaves(subroot, descendants)
+    dna_children_1, dna_children_2, together, dna_descendants, some = [], [], [], [], []
 
-    leaves = tree_utils.get_random_descendants(descendants)
-    together = tree_utils.are_together(leaves[0], leaves[1], subroot)
-    # print(leaves[0].name, ", ", leaves[1].name, " together: ", together, " subroot ", subroot.name)
+    for i in range(batchSize):
+        subroot = tree.get_random_node()
+        descendants = []
+        tree_utils.get_all_node_descendant_leaves(subroot, descendants)
 
-    dna_descendants = []
-    for child in descendants:
-        dna_descendants.append(np.reshape(data[child.name][0].astype(np.float32), (1, 20, 4)))
+        for child in descendants:
+            # dna_descendants.append(np.reshape(data[child.name][0].astype(np.float32), (1, 20, 4)))
+            dna_descendants.append(data[child.name][0].astype(np.float32))
 
-    dna_child_1 = data[leaves[0].name][0]
-    dna_child_2 = data[leaves[1].name][0]
+        some.append(dna_descendants)
 
-    return subroot, dna_descendants, dna_child_1, dna_child_2, together
+        leaves = tree_utils.get_random_descendants(descendants)
+        together.append([tree_utils.are_together(leaves[0], leaves[1], subroot)])
+        # print(leaves[0].name, ", ", leaves[1].name, " together: ", together, " subroot ", subroot.name)
+
+        dna_children_1.append(data[leaves[0].name][0])
+        dna_children_2.append(data[leaves[1].name][0])
+
+    return some, dna_children_1, dna_children_2, together
 
 
 def init_weights(shape):
@@ -77,12 +76,12 @@ def init_weights(shape):
     return tf.Variable(weights)
 
 
-tree = tree_parser.parse('dataset/small_tree.tree')
+tree = tree_parser.parse('dataset/phylogeny.tree')
 
-data_input = tf.placeholder(tf.float32, [None, 1, sequenceLength, dnaNumLetters])
-dna_sequence_input_1 = tf.placeholder(tf.float32, [1, sequenceLength, dnaNumLetters])
-dna_sequence_input_2 = tf.placeholder(tf.float32, [1, sequenceLength, dnaNumLetters])
-inputY = tf.placeholder(tf.int32, [1])
+data_input = tf.placeholder(tf.float32, [batchSize, None, sequenceLength, dnaNumLetters])
+dna_sequence_input_1 = tf.placeholder(tf.float32, [batchSize, sequenceLength, dnaNumLetters])
+dna_sequence_input_2 = tf.placeholder(tf.float32, [batchSize, sequenceLength, dnaNumLetters])
+inputY = tf.placeholder(tf.float32, [batchSize, 1])
 
 # Encoder
 encoder_cell = tf.nn.rnn_cell.BasicLSTMCell(hiddenUnits)
@@ -92,7 +91,8 @@ encoded_dataset = tf.map_fn(lambda x: tf.nn.dynamic_rnn(cell=encoder_cell,
                                                         dtype=tf.float32, time_major=False)[0][:, -1, :], data_input,
                             dtype=tf.float32)
 
-encoded_dataset = tf.reduce_mean(encoded_dataset, axis=0)
+encoded_dataset = tf.map_fn(lambda x: tf.reduce_mean(x, axis=0), encoded_dataset,
+                            dtype=tf.float32)
 
 encoder_outputs, _ = tf.nn.dynamic_rnn(cell=encoder_cell,
                                        inputs=dna_sequence_input_1, dtype=tf.float32)
@@ -108,15 +108,15 @@ feed_forward_inputX = tf.concat([encoded_dataset, encoded_dna_sequence_1, encode
 w_1 = init_weights((3 * hiddenUnits, hiddenUnits))
 b1 = tf.Variable(np.zeros((1, hiddenUnits)), dtype=tf.float32)
 
-w_2 = init_weights((hiddenUnits, 2))
-b2 = tf.Variable(np.zeros((1, 2)), dtype=tf.float32)
+w_2 = init_weights((hiddenUnits, 1))
+b2 = tf.Variable(np.zeros((1, 1)), dtype=tf.float32)
 
 h = tf.nn.tanh(tf.matmul(feed_forward_inputX, w_1) + b1)
 output = tf.matmul(h, w_2) + b2
 
 predictions = tf.nn.sigmoid(output)
 
-losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=inputY, logits=output)
+losses = tf.nn.sigmoid_cross_entropy_with_logits (labels=inputY, logits=output)
 total_loss = tf.reduce_mean(losses)
 
 training_alg = tf.train.AdagradOptimizer(0.02).minimize(total_loss)
@@ -124,26 +124,23 @@ training_alg = tf.train.AdagradOptimizer(0.02).minimize(total_loss)
 correct_pred = tf.equal(tf.round(predictions), tf.cast(inputY, tf.float32))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
-
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    data = read_data("dataset/small_tree_seq.txt")
+    data = read_data("dataset/dna_sequences_20.txt")
 
     for step in range(numTrainingIters + 1):
 
-        subroot, dna_descendants, dna_child_1, dna_child_2, together = get_subroot_and_nodes(tree, data)
+        dna_descendants, dna_child_1, dna_child_2, together = get_subroot_and_nodes(tree, data)
 
         _encoded_dataset, _totalLoss, _training_alg, _predictions, _accuracy = sess.run(
             [encoded_dataset, total_loss, training_alg, predictions, accuracy],
             feed_dict={
                 data_input: dna_descendants,
-                dna_sequence_input_1: [dna_child_1],
-                dna_sequence_input_2: [dna_child_2],
-                inputY: [together]
+                dna_sequence_input_1: dna_child_1,
+                dna_sequence_input_2: dna_child_2,
+                inputY: together
             })
 
-        if step % 500 == 0:
+        if step % 50 == 0:
             print("Step: {:5}\tLoss: {:.3f}\tAcc: {:.2%}".format(
                 step, _totalLoss, _accuracy))
-
-
